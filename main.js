a97eab48e094922caa11072001d5fa2b984ebc90
Kyle Kelley <rgbkrk@gmail.com>
2015-11-25 15:20:50 -0600

    Clean out unneccesary comment.

--- main.js
+++ main.js
@@ -21,9 +21,6 @@ app.on('ready', () => {
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null;
   });
 });
