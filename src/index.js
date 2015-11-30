46e40fe36e4ec2197d9a37c66aa6cc6a44801d7c
Kyle Kelley <rgbkrk@gmail.com>
2015-11-30 14:16:51 -0600

    Try out react-json-tree for a db viewer

--- src/index.js
+++ src/index.js
@@ -31,6 +31,32 @@ const {
   channels
 } = constants
 
+var React = require('react');
+var ReactDOM = require('react-dom');
+var JSONTree = require('react-json-tree');
+
+const theme = {
+  scheme: 'sogreen',
+  author: 'RobCo Industries (https://github.com/RobCoIndustries)',
+  base00: '#003000',
+  base01: '#203020',
+  base02: '#004000',
+  base03: '#204020',
+  base04: '#005000',
+  base05: '#205020',
+  base06: '#006000',
+  base07: '#206020',
+  base08: '#007000',
+  base09: '#207020',
+  base0A: '#008000',
+  base0B: '#208020',
+  base0C: '#009000',
+  base0D: '#00B000',
+  base0E: '#00D000',
+  base0F: '#00F000'
+};
+
+
 discover()
   .then(server => createSocket(server.info.address))
   .then(socket => {
@@ -40,110 +66,18 @@ discover()
   .then(subject => {
     connected(subject)
       .then(handshake => {
-        // Create Canvas for Fallout 4 Map
-        const canvas = document.getElementById('map')
-        const context = canvas.getContext('2d')
-
-        let image;
-        let empty;
-        let prev;
-
-        const arrow = document.getElementById('arrow')
-        arrow.style.left = '50%'
-        arrow.style.top = '50%'
-
-        canvas.style.WebkitFilter = 'hue-rotate(0deg)'
-
-        subject.onNext(['RequestLocalMapSnapshot'])
-        const localmap = subject
-          .filter(x => x.type === channels.LocalMapUpdate)
-          .throttle(1000 / 30) // 30 FPS hard limit
-          .doOnNext(x => {
-            subject.onNext(['RequestLocalMapSnapshot'])
-          })
-          .map(x => parseBinaryMap(x.payload))
-          .distinctUntilChanged()
-
-        const resize = Observable
-          .fromEvent(window, 'resize')
-          .merge(Observable.just())
-
-        localmap
-          .first()
-          .combineLatest(resize, x => ({
-            x: window.innerWidth / x.width,
-            y: window.innerHeight / x.height
-          }))
-          .subscribe(scale => {
-            canvas.style.transform = `scale(${scale.x},${scale.y})`
-            canvas.style.transformOrigin = '0 0'
-          })
-
-        localmap
-          .filter(map => {
-            if (!empty) {
-              empty = new Buffer(map.pixels.length)
-            }
-            return !map.pixels.equals(empty)
-          })
-          .map(map => {
-            const {
-              width,
-              height,
-              pixels
-            } = map
-
-            if (!image) {
-              image = context.createImageData(width, height)
-            }
-
-            const data = image.data;
-
-            for (let i = 0; i < pixels.length; i++) {
-              const val = pixels[i]
-              if (!prev || val !== prev[i]) {
-                const offset = i * 4
-                data[offset] = val
-                data[offset + 1] = val
-                data[offset + 2] = val
-                data[offset + 3] = 255
-              }
-            }
-
-            prev = pixels
-            return {
-              image,
-              map
-            }
-          })
-          .subscribe(res => {
-            const {
-              image,
-              map
-            } = res
-
-            canvas.height = map.height
-            canvas.width = map.width
-
-            context.globalCompositeOperation = 'source-over'
-            context.putImageData(image, 0, 0)
-            context.globalCompositeOperation = 'multiply'
-            context.fillStyle = 'rgb(25, 255, 25)'
-            context.fillRect(0, 0, map.width, map.height)
-          })
-
+        console.log("Connected");
         const database = subject
           .filter(x => x.type === channels.DatabaseUpdate)
           .map(x => parseBinaryDatabase(x.payload))
           .scan(aggregateBundles, {})
           .map(x => generateTreeFromDatabase(x))
 
+        // Update our database display
+        // TODO: Only update the db tree when in view
         database
-          .map(x => x.Map.World.Player)
-          .map(x => x.Rotation)
-          .distinctUntilChanged()
-          .subscribe(rotation => {
-            arrow.style.transform = `rotate(${rotation}deg)`
+          .subscribe(db => {
+            ReactDOM.render(<JSONTree data={ db } theme={ theme } />, document.getElementById('inspector'));
           })
       })
       .catch(err => {
