d2db761cfc50bfdef56fb71841c8ce324472af71
Luckydonald <luckydonald@luckydonaldsMBP.fritz.box>
2015-12-13 06:11:34 +0100

    Added Text color.
    This is much code for nothing.

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import { withStore } from 'fluorine-lib'
 
+import {
+  decoding
+} from 'pipboylib'
+
+import Database from '../stores/Database'
+import dispatcher from '../dispatcher'
+
+const { generateTreeFromDatabase } = decoding
+
+@withStore(dispatcher
+  .reduce(Database)
+  .map(x => generateTreeFromDatabase(x))
+  .filter(x => x && x.Status)
+  .map(x => x.Status.EffectColor)
+  .map(effectColor => {
+    let effectColors = effectColor.map(x => Math.round(x*255) )
+    let effect = {
+      red: effectColors[0],
+      green: effectColors[1],
+      blue: effectColors[2]
+    }
+    return `rgb(${effect.red},${effect.green},${effect.blue})`
+  })
+  .distinctUntilChanged(),
+  'color')
 export default class About extends React.Component {
   render() {
+    const styles = {
+      about: {
+        color: this.props.color
+      }
+    }
+
     return (
-      <div>
+      <div style={styles.about}>
         <h1>RobCo Industries PipBoy</h1>
         <p>Fan built precision electronics</p>
       </div>
