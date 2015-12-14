0ad992c29766295ae66a5bdeef92f375308c67f0
Kyle Kelley <rgbkrk@gmail.com>
2015-12-13 19:13:10 -0600

    Make note in our bootstrap main

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
+// Bootstrap main that lets us use ES6, JSX, etc.
+// Through the rest of our app code.
+// NOTE: This must remain ES5 code.
 require('electron-compile').init();
 require('./app.js');
